ada0dfdb88a2e2fbdbbdc6b0ef8cf6736822daed
Marvin Scholz <epirat07@gmail.com>
2015-02-12 18:02:07 +0100

    First working Version 1.0

---
+++ DaftPunKonsole/EPKeyCatcherView.h
@@ -0,0 +1,13 @@
+//
+//  EPKeyCatcherView.h
+//  DaftPunKonsole
+//
+//  Created by Marvin Scholz on 12.02.15.
+//  Copyright (c) 2015 ePirat. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface EPKeyCatcherView : NSView
+
+@end
---
+++ DaftPunKonsole/EPKeyCatcherView.m
@@ -0,0 +1,19 @@
+//
+//  EPKeyCatcherView.m
+//  DaftPunKonsole
+//
+//  Created by Marvin Scholz on 12.02.15.
+//  Copyright (c) 2015 ePirat. All rights reserved.
+//
+
+#import "EPKeyCatcherView.h"
+
+@implementation EPKeyCatcherView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
